91a71bca3d65e28820d4e4e785e6c6dc343fd73f
olivier <oc.chappe@gmail.com>
2014-04-21 16:49:39 +0200

    organize

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <limits>
 
+#include "Processing/processing.h"
+
 int main(int argc, char *argv[])
 {
 //    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
